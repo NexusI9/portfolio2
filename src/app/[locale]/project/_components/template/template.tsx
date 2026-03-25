6260983a8a1cab0c0818862612b1341d8584e7a7
NexusI9 <nassim.elkhantour@gmail.com>
2026-03-25 16:04:07 +0800

    fix responsive

--- src/app/[locale]/project/_components/template/template.tsx
+++ src/app/[locale]/project/_components/template/template.tsx
@@ -23,9 +23,11 @@ export default function Template({ project, children }: ITemplate) {
 	return (<>
 		<Banner {...banner} />
 		<div className="inline-block w-full">
-			<Container className={styles.container}>
-				<Sidebar {...sidebar} />
-				<Content>{children}</Content>
+			<Container className={styles.container} type="FLUID">
+				<div className={styles.inner}>
+					<Sidebar {...sidebar} />
+					<Content>{children}</Content>
+				</div>
 			</Container >
 		</div>
 	</>);
